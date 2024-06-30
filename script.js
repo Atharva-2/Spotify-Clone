5cdff523416e3021bdb9d5dc4a3f298b2297cf0c
Atharva-2 <atharvarasal202@gmail.com>
2024-06-30 12:16:00 +0530

    fourth day updated

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let currenSong = new Audio();
+let currentSong = new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "00:00";
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
@@ -16,15 +30,23 @@ async function getSongs() {
   return songs;
 }
 
-const playMusic = (track) => {
+const playMusic = (track, pause = false) => {
   //   let audio = new Audio("/songs/"+track);
-  currenSong.src = "/songs/" + track;
-  currenSong.play();
+  currentSong.src = "/songs/" + track;
+  if (!pause) {
+    currentSong.play();
+    play.src = "svgs/pause.svg";
+  }
+  document.querySelector(".songinfo").innerHTML = track;
+  document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(
+    currentSong.currentTime
+  )} / ${secondsToMinutesSeconds(currentSong.duration)}`;
 };
 
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
+  playMusic(songs[0], true);
+  // console.log(songs);
 
   let songUl = document
     .querySelector(".songList")
@@ -52,6 +74,49 @@ async function main() {
       console.log(e.querySelector(".info").firstElementChild.innerHTML);
       playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
     });
+  });
+
+  // const play = document.getElementById("play");
+
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "svgs/pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "svgs/play.svg";
+    }
+  });
+
+  currentSong.addEventListener("timeupdate", () => {
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(
+      currentSong.currentTime
+    )} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+
+    const circle = document.querySelector(".circle");
+    circle.style.left =
+      (currentSong.currentTime / currentSong.duration) * 99 + "%";
+  });
+
+  const seekbar = document.querySelector(".seekbar");
+
+  seekbar.addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
+  });
+
+  const hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    document.querySelector(".left").style.left = "0";
+  });
+
+  const close = document.querySelector(".close");
+
+  close.addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-120%";
   });
 }
 
